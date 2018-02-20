d25cbf9467ae5f9258d52721d534aa423df988b7
mobinet113 <benjaminroffe@mobiusit.net>
2018-02-20 17:09:32 +0000

    Crawl all pages and save to file

--- index.php
+++ index.php
@@ -1,29 +1,52 @@
 <?php
 
+$t    = time();
+$time = date("d-m-Y", $t);
+
 $crawler = new MWVCrawl;
 $crawler->getExhibitors();
+
+$crawler->fileLocation = 'output/exhibitors_' . $time . '.json';
+$crawler->saveToFile();
 
 class MWVCrawl {
 
   public $target = 'https://www.mobileworldcongress.com/exhibition/2018-exhibitors/';
+  public $fileLocation = 'output/exhibitors.json';
 
-  private $exhibitors = [];
+  private $exhibitorPageLinks  = [];
+  private $exhibitors          = [];
 
-  function _construct(){
+
+  /**
+   * Instantiate the class and grab all the companies and their websites.
+   */
+  public function getExhibitors(){
+
+    $this->consoleMessage("\r\n \r\n Getting Archive Links... \r\n");
+
+    $this->getArchiveLinks($this->target);
+
+    $this->consoleMessage("\r\n \r\n Getting Company Information... \r\n");
+
+    foreach( $this->exhibitorPageLinks as $pageLink ){
+      $this->exhibitors[] = $this->getCompany($pageLink);
+    }
 
   }
 
-  public function getExhibitors(){
-    $pageLinks = $this->getArchiveLinks($this->target);
+  /**
+   * Save the exhbitor list to file
+   */
+  public function saveToFile(){
 
-    foreach( $pageLinks as $pageLink ){
-      $company = $this->getCompany($pageLink);
+    $this->consoleMessage("Saving Company List To File \r\n");
+    $this->consoleMessage($this->fileLocation . "\r\n");
 
-      $this->exhibitors[] = $company;
-    }
+    $fp = fopen($this->fileLocation, 'w');
 
-    $this->debug($this->exhibitors);
-
+    fwrite( $fp, json_encode($this->exhibitors) );
+    fclose($fp);
   }
 
   private function debug($n){
@@ -32,6 +55,12 @@ class MWVCrawl {
     print_r('</pre>');
   }
 
+  /**
+   * Grabs a page by it's URL and turns it into a PHP DOM Object
+   *
+   * @param $url
+   * @return DOMDocument
+   */
   private function getPage($url){
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
@@ -47,6 +76,12 @@ class MWVCrawl {
     return $dom;
   }
 
+  /**
+   * Gets all the page links on the exhibitor list
+   *
+   * @param $url
+   * @return array
+   */
   private function getArchiveLinks($url){
 
     $returnLinks = [];
@@ -61,13 +96,41 @@ class MWVCrawl {
       $links = $element->getElementsByTagName('a');
 
       foreach ($links as $link) {
-        $returnLinks[] = $link->getAttribute('href');
+
+        $href = $link->getAttribute('href');
+
+        $this->consoleMessage("- " . $href . "\r\n");
+
+        $this->exhibitorPageLinks[] = $href;
+        $returnLinks[] = $href;
       }
     }
 
+    // If another page exists, then we need to get the links from that one too
+    $pagiNextBtn = $dom_xpath->query("//a[contains(@class, 'next') and contains(@class, 'page-numbers')]");
+
+    if ( $pagiNextBtn->length > 0 ){
+      $nextUrl = $pagiNextBtn[0]->attributes->getNamedItem("href")->nodeValue;
+
+      $this->getArchiveLinks($nextUrl);
+    }
+
+
     return $returnLinks;
+
   }
 
+  private function consoleMessage($msg){
+    echo $msg;
+    flush();
+  }
+
+  /**
+   * Gets the name and website of a company from it's exhibitor page
+   *
+   * @param $url
+   * @return array
+   */
   private function getCompany($url){
     $dom = $this->getPage($url);
 
@@ -83,12 +146,11 @@ class MWVCrawl {
     $elements = $dom_xpath->query("//a[@class='web-site-link']");
     $website  = $elements[0]->attributes->getNamedItem("href")->nodeValue;
 
+    $this->consoleMessage("- Name: " . $name    . "\r\n");
+    $this->consoleMessage("- Webs: " . $website . "\r\n");
 
     return array('name' => $name, 'website' => $website);
   }
 
-  private function goToNextPage(){
-
-  }
 
 }
